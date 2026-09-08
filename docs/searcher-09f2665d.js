9d97fff315d6a1f1ef7a93ad27a0ff59cd7067c5
jburrow <jaburrow@gmail.com>
2026-09-08 19:22:38 +0000

    docs: deploy gh-pages site for 0.12.1 @ 29a036f3075da4b00be3be3bb20d116daa4c293b

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1bc25194.js',
+                path_to_root + 'searchindex-6178e14e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
